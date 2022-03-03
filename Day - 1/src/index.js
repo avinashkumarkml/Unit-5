8847c13d2cd625737a73f0ae93a3f7c267329926
avinashkumarkml <96098283+avinashkumarkml@users.noreply.github.com>
2022-03-03 16:51:05 +0530

    add code

---
+++ Day - 1/src/index.js
@@ -0,0 +1,14 @@
+import "./index.css"
+
+const h1 = document.createElement("h1");
+h1.textContent = "My name is avinash"
+h1.classList.add("list")
+document.getElementById("maindiv").append(h1)
+
+var img = document.createElement("img")
+img.src = "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202203/viratkohliicctestrankings_1200x768.jpeg?ignpJy2viwXjFJ.mEHjLPo34Qsxdy_kg&size=770:433"
+img.classList.add("list")
+document.getElementById("maindiv").append(img);
+
+
+
