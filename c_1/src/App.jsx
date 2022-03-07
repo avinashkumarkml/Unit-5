853dce4f2b5c79225159d302bc2ea27b3562bbec
avinashkumarkml <96098283+avinashkumarkml@users.noreply.github.com>
2022-03-07 10:48:23 +0530

    add

--- c_1/src/App.jsx
+++ c_1/src/App.jsx
@@ -1,6 +1,5 @@
-
 import './App.css';
-import {Inventry} from "./components/count";
+import {Inventry} from "./components/count"
 
 function App() {
   return (
--- c_1/src/components/count.jsx
+++ c_1/src/components/count.jsx
@@ -1,18 +1,63 @@
-import { useState} from "react"
+import { useState } from "react";
 
 export function Inventry(){
 
     const [counter, setCounter]=useState(0);
+
+    const [pen, setPen]=useState(0);
+
+    const [notebook, setNotebook]=useState(0);
+   
+    const [inkpen, setInkpen]=useState(0);
+   
     const changeCount=(value)=>{
-        setCounter(counter+value)
+        setCounter(counter+value);
     };
 
+    const changePen=(value)=>{
+        setPen(pen+value);
+    };
+
+    const changeNotebooks=(value)=>{
+        setNotebook(notebook+value);
+    };
+
+    const changeInkpen=(value)=>{
+        setInkpen(inkpen+value);
+    };
+
+
+
     return(
-        <><div className="items">
-        <span>Books:{counter}</span>
-        <button className="addBook" onclick={()=>changeCount(1)}>+</button>
-        <button className="remBook" onclick={()=>changeCount(-1)}>-</button>
+       <> <div className="items">
+            <div><span className="totalBooks">Books:{counter}</span>
+            <button className="addBook" onClick={()=>changeCount(1)}>+</button>
+            <button className="remBook" onClick={()=>{if (counter>=1){changeCount(-1)}}}>-</button>
+            </div>
+
+
+            <div><span className="totalPens">Pens:{pen}</span>
+            <button className="addPen" onClick={()=>changePen(1)}>+</button>
+            <button className="remPen" onClick={()=>{if (pen>=1){changePen(-1)}}}>-</button>
+            </div>
+
+
+            <div><span className="totalNotebooks">Notebooks:{notebook}</span>
+            <button className="addNotebook" onClick={()=>changeNotebooks(1)}>+</button>
+            <button className="remNotebook" onClick={()=>{if (notebook>=1){changeNotebooks(-1)}}}>-</button>
+            </div>
+
+
+            <div><span className="totalInkpens">Inkpen:{inkpen}</span>
+            <button className="addInkpen" onClick={()=>changeInkpen(1)}>+</button>
+            <button className="remInkpen" onClick={()=>{if (inkpen>=1){changeInkpen(-1)}}}>-</button>
+            </div>
+
+
+
+
         </div>
         </>
     )
+
 }
