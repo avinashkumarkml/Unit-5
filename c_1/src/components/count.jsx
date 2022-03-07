78df666fc58fcd080d3940cc80da3363f9242e0c
avinashkumarkml <96098283+avinashkumarkml@users.noreply.github.com>
2022-03-07 10:30:14 +0530

    add

---
+++ c_1/src/components/count.jsx
@@ -0,0 +1,18 @@
+import { useState} from "react"
+
+export function Inventry(){
+
+    const [counter, setCounter]=useState(0);
+    const changeCount=(value)=>{
+        setCounter(counter+value)
+    };
+
+    return(
+        <><div className="items"></div>
+        <span>Books:{counter}</span>
+        <button className="addBook" onclick={()=>changeCount(1)}>+</button>
+        <button className="remBook" onclick={()=>changeCount(-1)}>-</button>
+        
+        </>
+    )
+}
